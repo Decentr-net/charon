7bd31b97d612fa2578a688b1fabbcddd491ba4a2
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2020-08-13 10:43:43 +0300

    inlineSVG install

--- charon/src/app/app.module.ts
+++ charon/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     SharedModule
   ],
   declarations: [
--- charon/src/app/shared/shared.module.ts
+++ charon/src/app/shared/shared.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material/material.module';
+import { InlineSVGModule } from 'ng-inline-svg';
 
 @NgModule({
   imports: [
     CommonModule,
+    InlineSVGModule.forRoot(),
     MaterialModule
   ],
   exports: [
+    InlineSVGModule,
     MaterialModule
   ]
 })
