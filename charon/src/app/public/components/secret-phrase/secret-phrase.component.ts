7fd35b0b321ec0c5b006ce890db9b7177f235dd0
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2020-09-02 18:29:35 +0300

    confirm secret phrase component implemented

--- charon/src/app/public/components/secret-phrase/secret-phrase.component.ts
+++ charon/src/app/public/components/secret-phrase/secret-phrase.component.ts
@@ -1,16 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export enum SeedPhrasePages {
+  GET_PHRASE = 'getPhrase',
+  CONFIRM_PHRASE = 'confirmPhrase',
+  CONGRATULATIONS = 'congratulations'
+}
 
 @Component({
   selector: 'app-secret-phrase',
   templateUrl: './secret-phrase.component.html',
   styleUrls: ['./secret-phrase.component.scss']
 })
-export class SecretPhraseComponent {
+export class SecretPhraseComponent implements OnInit {
+
+  isPageVisible = {
+    [SeedPhrasePages.GET_PHRASE]: true,
+    [SeedPhrasePages.CONFIRM_PHRASE]: false,
+    [SeedPhrasePages.CONGRATULATIONS]: false
+  };
 
   isSeedPhraseVisible = false;
+  isSelectedSeedPhraseValid = false;
   seedPhrase = 'enemy money update snake wood soda depend shine visit lion frequent two';
 
-  showBackupPhrase() {
+  seedPhraseShuffledArr = [];
+  selectedSeedPhraseArr = [];
+
+  ngOnInit() {
+    this.seedPhraseShuffledArr = this.shuffleArray(this.seedPhrase.split(' ').slice());
+  }
+
+  onSwitchPage(page: string) {
+    for (let key in this.isPageVisible) {
+      this.isPageVisible[key] = key === page;
+    }
+  }
+
+  showSeedPhrase() {
     this.isSeedPhraseVisible = true;
+  }
+
+  onSelectWord(i: number, from, to) {
+    to.push(from.splice(i, 1));
+
+    this.checkSelectedPhrase();
+  }
+
+  checkSelectedPhrase() {
+    this.isSelectedSeedPhraseValid = this.selectedSeedPhraseArr.join(' ') === this.seedPhrase;
+  }
+
+  shuffleArray(array) {
+    return array.sort(() => Math.random() - 0.5);
+  }
+
+  get SeedPhrasePages() {
+    return SeedPhrasePages;
   }
 }
