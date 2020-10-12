d6d4ced65dc7ad6cbf4b814066905beef47a6169
eugene.tishkevich <eugene.tishkevich@gmail.com>
2020-10-12 20:52:08 +0300

    local store: use queue to avoid concurrency

--- charon/src/app/shared/services/local-store/chrome/chrome-store.service.ts
+++ charon/src/app/shared/services/local-store/chrome/chrome-store.service.ts
@@ -1,30 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import PQueue from 'p-queue';
 
 import { LocalStoreService } from '../local-store.service';
 
 @Injectable()
 export class ChromeStoreService extends LocalStoreService {
+  private readonly queue = new PQueue({ concurrency: 1 });
   private chromeStorage = chrome.storage.local;
 
   public static canUse = !!window.chrome && window.chrome.storage;
 
   public get<T>(key: string): Promise<T> {
-    return new Promise<T>(resolve => {
+    return this.queue.add(() => new Promise<T>(resolve => {
       this.chromeStorage.get(key, obj => resolve(obj[key]));
-    });
+    }));
   }
 
-  public set<T>(key: string, value: T): Promise<void> {
-    return new Promise<void>(resolve => {
+  public async set<T>(key: string, value: T): Promise<void> {
+    return this.queue.add(() => new Promise<void>(resolve => {
       this.chromeStorage.set({ [key]: value }, () => resolve());
-    });
+    }));
   }
 
   public remove(key: string): Promise<void> {
-    return new Promise<void>(resolve => {
+    return this.queue.add(() =>  new Promise<void>(resolve => {
       this.chromeStorage.remove(key, () => resolve());
-    });
+    }));
   }
 
   public onChange<T>(key: string): Observable<T> {
