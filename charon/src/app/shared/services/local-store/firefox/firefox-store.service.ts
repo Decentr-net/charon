9f782d347436de7a9d2a6b25ba72e94c7d91ceb7
eugene.tishkevich <eugene.tishkevich@gmail.com>
2020-10-12 20:52:08 +0300

    local store: use queue to avoid concurrency

--- charon/src/app/shared/services/local-store/firefox/firefox-store.service.ts
+++ charon/src/app/shared/services/local-store/firefox/firefox-store.service.ts
@@ -1,24 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import PQueue from 'p-queue';
 
 import { LocalStoreService } from '../local-store.service';
 
 @Injectable()
 export class FirefoxStoreService extends LocalStoreService {
+  private readonly queue = new PQueue({ concurrency: 1 });
   private firefoxStorage = window['browser'].storage.local;
 
   public static canUse = !!window['browser'] && window['browser'].storage;
 
   public get<T>(key: string): Promise<T> {
-    return this.firefoxStorage.get(key).then(obj => obj[key]);
+    return this.queue.add(() => this.firefoxStorage.get(key).then(obj => obj[key]));
   }
 
   public set<T>(key: string, value: T): Promise<void> {
-    return this.firefoxStorage.set({ [key]: value });
+    return this.queue.add(() => this.firefoxStorage.set({ [key]: value }));
   }
 
   public remove(key: string): Promise<void> {
-    return this.firefoxStorage.remove(key);
+    return this.queue.add(() => this.firefoxStorage.remove(key));
   }
 
   public onChange<T>(key: string): Observable<T> {
