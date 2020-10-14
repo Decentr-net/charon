31f5f938994d49ddef4ac2eacd76098ebe00b09a
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2020-10-14 14:27:47 +0300

    get currency rate

--- charon/src/environments/environment.prod.ts
+++ charon/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 import { Environment } from './environment.definitions';
 
 export const environment: Environment = {
+  currencyApi: 'https://api.coingecko.com/api/v3',
   production: true,
   restApi: 'https://rest.testnet.decentr.xyz',
   vulcanApi: 'https://vulcan.testnet.decentr.xyz/v1',
