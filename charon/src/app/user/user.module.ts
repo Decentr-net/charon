9e82ecaf9dd133c9842ff44e94414a204825a0af
Eugene <eugene.tishkevich@gmail.com>
2020-10-19 12:25:43 +0300

    feature: edit profile (#26)
    * feature: user: account details
    
    * remove debug code

--- charon/src/app/user/user.module.ts
+++ charon/src/app/user/user.module.ts
@@ -1,26 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { InlineSVGModule } from 'ng-inline-svg';
 import { TRANSLOCO_SCOPE, TranslocoModule } from '@ngneat/transloco';
 
+import { DateInputModule } from '@shared/components/date-input';
+import { FormErrorModule } from '@shared/components/form-error';
 import { LayoutHeaderModule } from '@shared/components/layout-header';
 import { USER_PAGES } from './pages';
 import { USER_COMPONENTS } from './components';
 import { UserRoutingModule } from './user-routing.module';
-import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   imports: [
     CommonModule,
+    DateInputModule,
+    FormErrorModule,
     InlineSVGModule,
     LayoutHeaderModule,
+    MatButtonModule,
     MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatMenuModule,
+    MatRadioModule,
     MatTabsModule,
     MatTooltipModule,
+    ReactiveFormsModule,
     TranslocoModule,
     UserRoutingModule,
   ],
