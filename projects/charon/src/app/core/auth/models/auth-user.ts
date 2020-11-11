b78405da99d8a90c7fe96f90eea2bf1d16a54b50
eugene.tishkevich <eugene.tishkevich@gmail.com>
2020-11-11 17:47:43 +0300

    auth: remove extra properties from AuthUser interface

--- projects/charon/src/app/core/auth/models/auth-user.ts
+++ projects/charon/src/app/core/auth/models/auth-user.ts
@@ -2,10 +2,8 @@ import { User } from '@root-shared/services/auth';
 
 export interface AuthUser extends User {
   readonly emailConfirmed: boolean;
-  readonly primaryEmail: string;
   readonly primaryUsername?: string;
   readonly passwordHash: string;
-  readonly registrationCompleted: boolean;
 }
 
 export type AuthUserCreate = Partial<AuthUser> & Pick<AuthUser, 'primaryEmail'> & { password: string };
