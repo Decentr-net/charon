4c25f0710ae6dac40b340202a291ff8face5462b
eugene.tishkevich <eugene.tishkevich@gmail.com>
2020-11-18 17:49:08 +0300

    feature: decentr circle: top level navigation

--- projects/charon/src/app/circle/pages/index.ts
+++ projects/charon/src/app/circle/pages/index.ts
@@ -1,7 +1,13 @@
 import { CirclePageComponent } from './circle-page';
+import { NewsPageComponent } from './news-page';
+import { OverviewPageComponent } from './overview-page';
 
 export * from './circle-page';
+export * from './news-page';
+export * from './overview-page';
 
 export const CIRCLE_PAGES = [
   CirclePageComponent,
+  NewsPageComponent,
+  OverviewPageComponent,
 ];
---
+++ projects/charon/src/app/circle/pages/overview-page/overview-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overview-page',
+  templateUrl: './overview-page.component.html',
+  styleUrls: ['./overview-page.component.scss']
+})
+export class OverviewPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
