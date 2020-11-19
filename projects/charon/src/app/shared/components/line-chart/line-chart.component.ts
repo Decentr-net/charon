1d8a704d6a84c27cbad1d0683b6312ec675231e6
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2020-11-19 23:51:30 +0300

    line-chart created

---
+++ projects/charon/src/app/shared/components/line-chart/line-chart.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-line-chart',
+  templateUrl: './line-chart.component.html',
+  styleUrls: ['./line-chart.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LineChartComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+}
---
+++ projects/charon/src/app/shared/components/line-chart/line-chart.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { LineChartComponent } from './line-chart.component';
+
+@NgModule({
+  declarations: [
+    LineChartComponent
+  ],
+  exports: [
+    LineChartComponent
+  ],
+})
+export class LineChartModule {
+}
