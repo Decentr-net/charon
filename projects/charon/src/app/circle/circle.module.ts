68e3c91f7219988a0e136ff58eeb737e8bc9a361
eugene.tishkevich <eugene.tishkevich@gmail.com>
2020-11-23 03:11:16 +0300

    feature: decentr circle: post dialog

--- projects/charon/src/app/circle/circle.module.ts
+++ projects/charon/src/app/circle/circle.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { TRANSLOCO_SCOPE, TranslocoModule } from '@ngneat/transloco';
 import { InlineSVGModule } from 'ng-inline-svg';
@@ -8,14 +9,16 @@ import { ColoredDistributionLineModule } from '../shared/components/colored-dist
 import { ColorLabelModule } from '../shared/components/color-label';
 import { TextEllipsisModule } from '../shared/directives/text-ellipsis';
 import { SanitizeModule } from '../shared/pipes/sanitize';
+import { SpinnerModule } from '../core/spinner';
 import { CIRCLE_COMPONENTS } from './components';
+import { CIRCLE_DIRECTIVES } from './directives';
 import { CIRCLE_PAGES } from './pages';
 import { CircleRoutingModule } from './circle-routing.module';
-import { SpinnerModule } from '../core/spinner';
 
 @NgModule({
   declarations: [
     CIRCLE_COMPONENTS,
+    CIRCLE_DIRECTIVES,
     CIRCLE_PAGES,
   ],
   imports: [
@@ -24,6 +27,7 @@ import { SpinnerModule } from '../core/spinner';
     ColorLabelModule,
     CommonModule,
     InlineSVGModule,
+    MatDialogModule,
     MatDividerModule,
     SanitizeModule,
     SpinnerModule,
