3f6fac87b61786507d3be389b55b37fe7498252a
eugene.tishkevich <eugene.tishkevich@gmail.com>
2020-11-24 21:53:39 +0300

    feature: color label: add hole mode

--- projects/charon/src/app/shared/components/color-label/color-label.component.ts
+++ projects/charon/src/app/shared/components/color-label/color-label.component.ts
@@ -8,4 +8,6 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 })
 export class ColorLabelComponent {
   @Input() public color: string;
+
+  @Input() public circleType: 'filled' | 'hole' = 'filled';
 }
--- projects/charon/src/app/shared/components/color-label/color-label.module.ts
+++ projects/charon/src/app/shared/components/color-label/color-label.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { ColorLabelComponent } from './color-label.component';
 
 @NgModule({
+  imports: [
+    CommonModule,
+  ],
   declarations: [
     ColorLabelComponent,
   ],
