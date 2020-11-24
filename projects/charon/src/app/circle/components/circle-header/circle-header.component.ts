71612cf1cf5499c20dea0c2955c57db428f86443
eugene.tishkevich <eugene.tishkevich@gmail.com>
2020-11-24 17:10:19 +0300

    feature: decentr circle: header bar

--- projects/charon/src/app/circle/components/circle-header/circle-header.component.ts
+++ projects/charon/src/app/circle/components/circle-header/circle-header.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-circle-header',
@@ -7,4 +10,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CircleHeaderComponent {
+  constructor(
+    h: HttpClient,
+    domSanitizer: DomSanitizer,
+    matIconRegistry: MatIconRegistry,
+  ) {
+    // h.get('assets/images/icons/notification.svg').subscribe(console.log);
+    matIconRegistry.addSvgIcon(
+      'notification1',
+      domSanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/notification.svg'),
+    );
+  }
 }
