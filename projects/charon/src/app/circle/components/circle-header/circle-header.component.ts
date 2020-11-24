e25aa50bcf4af3f233ad637b14d0d2392214bf03
Eugene <eugene.tishkevich@gmail.com>
2020-11-24 22:05:56 +0300

    feature: decentr circle: header bar (#56)

--- projects/charon/src/app/circle/components/circle-header/circle-header.component.ts
+++ projects/charon/src/app/circle/components/circle-header/circle-header.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+import { AppRoute } from '../../../app-route';
+import { UserRoute } from '../../../user';
+import { CircleRoute, CircleWallRoute } from '../../circle-route';
+
+export const CIRCLE_HEADER_META_SLOT = Symbol('CIRCLE_HEADER_META_SLOT');
+export const CIRCLE_HEADER_CONTENT_SLOT = Symbol('CIRCLE_HEADER_CONTENT_SLOT');
 
 @Component({
   selector: 'app-circle-header',
@@ -7,4 +14,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CircleHeaderComponent {
+  @Input() public avatarSrc: string;
+
+  public readonly metaSlotName: Symbol = CIRCLE_HEADER_META_SLOT;
+  public readonly contentSlotName: Symbol = CIRCLE_HEADER_CONTENT_SLOT;
+
+  public readonly appRoute: typeof AppRoute = AppRoute;
+  public readonly circleRoute: typeof CircleRoute = CircleRoute;
+  public readonly circleWallRoute: typeof CircleWallRoute = CircleWallRoute;
+  public readonly userRoute: typeof UserRoute = UserRoute;
 }
--- projects/charon/src/app/circle/pages/circle-page/circle-page.component.ts
+++ projects/charon/src/app/circle/pages/circle-page/circle-page.component.ts
@@ -1,10 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+import { CIRCLE_HEADER_CONTENT_SLOT, CIRCLE_HEADER_META_SLOT } from '../../components/circle-header';
+import { CirclePageService } from './circle-page.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-circle-page',
   templateUrl: './circle-page.component.html',
   styleUrls: ['./circle-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    CirclePageService,
+  ],
 })
-export class CirclePageComponent {
+export class CirclePageComponent implements OnInit {
+  public headerMetaSlotName = CIRCLE_HEADER_META_SLOT;
+  public headerContentSlotName = CIRCLE_HEADER_CONTENT_SLOT;
+
+  public avatar: string = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1200px-Square_-_black_simple.svg.png';
+
+  public coinRate$: Observable<number>;
+  public balance$: Observable<string>;
+
+  constructor(
+    private circlePageService: CirclePageService,
+  ) {
+  }
+
+  public ngOnInit(): void {
+    this.coinRate$ = this.circlePageService.getCoinRate();
+
+    this.balance$ = this.circlePageService.getBalance();
+  }
 }
