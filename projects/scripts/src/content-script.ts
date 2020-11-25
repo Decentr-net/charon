659b8c9b59bcae821874a96c0130dfd64d2aa5cc
eugene.tishkevich <eugene.tishkevich@gmail.com>
2020-11-26 00:29:48 +0300

    toolbar

--- projects/scripts/src/content-script.ts
+++ projects/scripts/src/content-script.ts
@@ -1,6 +1,9 @@
+import { take } from 'rxjs/operators';
+
+import { MessageBus } from '../../../shared/message-bus';
+import { TOOLBAR_CLOSE } from '../../toolbar/src/app/messages';
 import { isTopWindow } from './helpers/window';
-import { createToolbarIframe } from './content/iframe';
-import { browser } from 'webextension-polyfill-ts';
+import { createToolbarIframe } from './content/toolbar';
 
 const iframe = createToolbarIframe();
 
@@ -8,4 +11,8 @@ if (isTopWindow(window.self)) {
   document.documentElement.append(iframe);
 }
 
-console.log(browser.extension.getURL('charon/index.html#/circle'));
+new MessageBus().onMessage(TOOLBAR_CLOSE)
+  .pipe(
+    take(1),
+  )
+  .subscribe(() => iframe.remove());
--- projects/toolbar/src/app/app.component.ts
+++ projects/toolbar/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { MessageBus } from '@shared/message-bus';
 import { AppRoute as CharonAppRoute } from '@charon/app-route';
 import { UserRoute as CharonUserRoute } from '@charon/user';
 import {
@@ -7,6 +8,7 @@ import {
   CircleWallRoute as CharonCircleWallRoute,
 } from '@charon/circle';
 import { openCharonPage } from './utils/extension';
+import { TOOLBAR_CLOSE } from './messages';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +17,10 @@ import { openCharonPage } from './utils/extension';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+  private readonly messageBus = new MessageBus();
+
   public close(): void {
+    this.messageBus.sendMessageToCurrentTab(TOOLBAR_CLOSE);
   }
 
   public openCharonNews(): void {
