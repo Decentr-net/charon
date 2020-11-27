c5764c720506b931fea8f906d9e05e900583fbe1
eugene.tishkevich <eugene.tishkevich@gmail.com>
2020-11-27 04:06:01 +0300

    show / hide toolbar on auth status change

--- projects/scripts/src/content-script.ts
+++ projects/scripts/src/content-script.ts
@@ -1,24 +1,30 @@
-import { take } from 'rxjs/operators';
+import { filter, take, takeUntil } from 'rxjs/operators';
 
 import { MessageBus } from '../../../shared/message-bus';
 import { TOOLBAR_CLOSE } from '../../toolbar/src/app/messages';
 import { isTopWindow } from './helpers/window';
-import { createToolbarIframe } from './content/toolbar';
+import { isAuthorized$ } from './content/auth';
 import { isToolbarClosed, saveToolbarClosed } from './content/session';
+import { createToolbarIframe } from './content/toolbar';
 
 if (!isToolbarClosed()) {
   const iframe = createToolbarIframe();
 
-  if (isTopWindow(window.self)) {
-    document.documentElement.append(iframe);
-  }
+  const close$ = new MessageBus().onMessage(TOOLBAR_CLOSE).pipe(
+    take(1),
+  );
 
-  new MessageBus().onMessage(TOOLBAR_CLOSE)
-    .pipe(
-      take(1),
-    )
-    .subscribe(() => {
-      iframe.remove();
-      saveToolbarClosed();
-    });
+  close$.subscribe(() => {
+    iframe.remove();
+    saveToolbarClosed();
+  });
+
+  isAuthorized$().pipe(
+    filter(() => isTopWindow(window.self)),
+    takeUntil(close$),
+  ).subscribe((isAuthorized) => {
+    isAuthorized
+      ? document.documentElement.append(iframe)
+      : iframe.remove();
+  });
 }
