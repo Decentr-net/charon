6e419f13e7ce4ab7a956cb4df034ab163c6a66db
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2020-11-30 13:38:48 +0300

    adjust import paths

--- projects/charon/src/app/user/user.module.ts
+++ projects/charon/src/app/user/user.module.ts
@@ -20,7 +20,6 @@ import { LayoutHeaderModule } from '@shared/components/layout-header';
 import { USER_PAGES } from './pages';
 import { USER_COMPONENTS } from './components';
 import { UserRoutingModule } from './user-routing.module';
-import { ChooseAvatarRadioModule } from '../shared/components/choose-avatar-radio';
 
 @NgModule({
   imports: [
