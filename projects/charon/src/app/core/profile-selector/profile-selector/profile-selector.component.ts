870a1fb6e76e401b6b487188898ce94f9efed607
Alex Kh <8222330+sunzxs@users.noreply.github.com>
2020-11-30 22:02:57 +0300

    feature: update user profile selector menu (#70)
    * extend public profile firstName, lastName
    
    * implement choose-avatar-radio, extend complete-registration by avatar
    
    * extend public for EditProfile
    
    * adjust user-avatar definition, profile-selector implement user-avatar
    
    * add decentr-hub to profile-selector menu

--- projects/charon/src/app/core/profile-selector/profile-selector/profile-selector.component.ts
+++ projects/charon/src/app/core/profile-selector/profile-selector/profile-selector.component.ts
@@ -5,6 +5,8 @@ import { pluck, shareReplay } from 'rxjs/operators';
 import { AuthService } from '../../auth';
 import { LockService } from '../../lock';
 import { ProfileSelectorService } from '../profile-selector.service';
+import { CircleRoute } from '../../../circle/circle-route';
+import { AppRoute } from '../../../app-route';
 
 @Component({
   selector: 'app-profile-selector',
@@ -14,6 +16,9 @@ import { ProfileSelectorService } from '../profile-selector.service';
 export class ProfileSelectorComponent implements OnInit {
   public balance$: Observable<string>;
   public username$: Observable<string>;
+  public userAvatar;
+
+  public readonly appRoute: typeof AppRoute = AppRoute;
 
   constructor(
     private authService: AuthService,
@@ -31,6 +36,8 @@ export class ProfileSelectorComponent implements OnInit {
       pluck('primaryUsername'),
       shareReplay(1),
     );
+
+    this.userAvatar = this.authService.getActiveUserInstant().avatar;
   }
 
   public lock(): void {
