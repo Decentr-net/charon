13fdd82db61f36550247e239b4958ed47a0233a3
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2020-11-30 13:28:11 +0300

    extend public for EditProfile

--- projects/charon/src/app/user/user.module.ts
+++ projects/charon/src/app/user/user.module.ts
@@ -20,6 +20,7 @@ import { LayoutHeaderModule } from '@shared/components/layout-header';
 import { USER_PAGES } from './pages';
 import { USER_COMPONENTS } from './components';
 import { UserRoutingModule } from './user-routing.module';
+import { ChooseAvatarRadioModule } from '../shared/components/choose-avatar-radio';
 
 @NgModule({
   imports: [
