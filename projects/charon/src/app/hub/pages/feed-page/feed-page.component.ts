6bcb2f2e37413d871a414e051e2a18579010885d
Eugene <eugene.tishkevich@gmail.com>
2020-12-03 21:22:32 +0300

    feature: hub: top feed (#76)

--- projects/charon/src/app/hub/pages/feed-page/feed-page.component.ts
+++ projects/charon/src/app/hub/pages/feed-page/feed-page.component.ts
@@ -1,10 +1,38 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, TrackByFunction } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from 'decentr-js';
+
+import { FeedPageService } from './feed-page.service';
 
 @Component({
   selector: 'app-feed-page',
   templateUrl: './feed-page.component.html',
   styleUrls: ['./feed-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    FeedPageService,
+  ],
 })
-export class FeedPageComponent {
+export class FeedPageComponent implements OnInit {
+  public isLoading$: Observable<boolean>;
+  public posts$: Observable<Post[]>;
+
+  private readonly loadingCount: number = 20;
+
+  constructor(private feedPageService: FeedPageService) {
+  }
+
+  public ngOnInit() {
+    this.posts$ = this.feedPageService.posts$;
+
+    this.isLoading$ = this.feedPageService.isLoading$;
+
+    this.loadMore();
+  }
+
+  public loadMore(): void {
+    this.feedPageService.loadMorePosts(this.loadingCount);
+  }
+
+  public trackByPostId: TrackByFunction<Post> = ({}, { uuid }) => uuid;
 }
---
+++ projects/charon/src/app/hub/pages/feed-page/feed-page.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { Post } from 'decentr-js';
+import { Observable } from 'rxjs';
+
+import { NetworkSelectorService } from '@core/network-selector';
+import { PostsApiService } from '@core/services/api';
+import { HubPostsService } from '../../services';
+
+@Injectable()
+export class FeedPageService extends HubPostsService implements OnDestroy {
+  constructor(
+    private networkService: NetworkSelectorService,
+    private postsApiService: PostsApiService,
+  ) {
+    super();
+  }
+
+  public ngOnDestroy() {
+    this.dispose();
+  }
+
+  protected loadPosts(fromPost: Post | undefined, count: number): Observable<Post[]> {
+    return this.postsApiService.getPopularPosts(
+      this.networkService.getActiveNetworkInstant().api,
+      'day',
+      {
+        fromOwner: fromPost && fromPost.owner,
+        fromUUID: fromPost && fromPost.uuid,
+        limit: count,
+      }
+    );
+  }
+}
