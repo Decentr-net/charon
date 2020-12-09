b143210c5c4b838e04e23287036a7838f0c0de7b
Eugene <eugene.tishkevich@gmail.com>
2020-12-09 04:25:41 +0300

    feature: hub: overview page posts (#83)

--- projects/charon/src/app/hub/pages/overview-page/overview-page.service.ts
+++ projects/charon/src/app/hub/pages/overview-page/overview-page.service.ts
@@ -1,5 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from 'decentr-js';
+
+import { PostsApiService } from '@core/services/api';
+import { NetworkService, UserService } from '@core/services';
+import { HubPostsService } from '../../services';
 
 @Injectable()
-export class OverviewPageService {
+export class OverviewPageService extends HubPostsService {
+  constructor(
+    private networkService: NetworkService,
+    private postsApiService: PostsApiService,
+    userService: UserService,
+  ) {
+    super(userService);
+  }
+
+  public ngOnDestroy() {
+    this.dispose();
+  }
+
+  protected loadPosts(fromPost: Post | undefined, count: number): Observable<Post[]> {
+    const api = this.networkService.getActiveNetworkInstant().api;
+
+    return this.postsApiService.getPopularPosts(api, 'month', {
+      limit: count,
+      fromOwner: fromPost && fromPost.owner,
+      fromUUID: fromPost && fromPost.uuid,
+    });
+  }
 }
