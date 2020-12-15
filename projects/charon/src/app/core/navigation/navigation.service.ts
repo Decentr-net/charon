812155a7fbe95147c6acd45e5642fbb6b6c799ff
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2020-12-15 22:41:04 +0300

    DEC-45: navigation service: add backIfNoHistory()

--- projects/charon/src/app/core/navigation/navigation.service.ts
+++ projects/charon/src/app/core/navigation/navigation.service.ts
@@ -1,16 +1,41 @@
 import { Injectable } from '@angular/core';
+import { filter, map } from 'rxjs/operators';
 import { Location } from '@angular/common';
-import { Tabs } from 'webextension-polyfill-ts';
-
+import { Observable } from 'rxjs';
 import { openExtensionInNewTab } from '@core/browser';
+import { Router, RoutesRecognized } from '@angular/router';
+import { Tabs } from 'webextension-polyfill-ts';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Injectable()
 export class NavigationService {
-  constructor(private location: Location) {
+  private previousPageUrl: string = undefined;
+
+  constructor(
+    private location: Location,
+    private router: Router,
+  ) {
+    this.getUrlChanges().pipe(
+      untilDestroyed(this),
+    ).subscribe(url => this.previousPageUrl = url);
   }
 
   public back(): void {
     this.location.back();
+  }
+
+  public backIfNoHistory(url: string[]): void {
+    const navigateUrl = this.previousPageUrl ? [this.previousPageUrl] : url;
+
+    this.router.navigate(navigateUrl);
+  }
+
+  private getUrlChanges(): Observable<string> {
+    return this.router.events.pipe(
+      filter(e => e instanceof RoutesRecognized),
+      map(() => this.router.url),
+    )
   }
 
   public openInNewTab(url: string): Promise<Tabs.Tab> {
