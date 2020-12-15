0dde2635a0d28750aa25d9b62b217a16cbd2a52a
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2020-12-16 01:56:48 +0300

    DEC-42: add provider DecimalPipe for PdvValueModule

--- shared/pipes/pdv-value/pdv-value.module.ts
+++ shared/pipes/pdv-value/pdv-value.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
 
 import { PdvValuePipe } from './pdv-value.pipe';
 
 @NgModule({
   declarations: [
     PdvValuePipe,
+  ],
+  providers: [
+    DecimalPipe,
   ],
   exports: [
     PdvValuePipe,
