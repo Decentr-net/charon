58dc8662d409d5f817c04fe6bc6daadd16b532a3
Eugene <eugene.tishkevich@gmail.com>
2020-12-16 18:58:19 +0300

    feature: toolbar toggle (#100)

--- projects/charon/src/app/user/user.module.ts
+++ projects/charon/src/app/user/user.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
@@ -16,6 +17,7 @@ import { TRANSLOCO_SCOPE, TranslocoModule } from '@ngneat/transloco';
 import { FormErrorModule } from '@shared/components/form-error';
 import { NavigationModule } from '@core/navigation';
 import { ProfileFormModule } from '@shared/components/profile-form';
+import { ToolbarStateService } from '@shared/services/toolbar-state';
 import { LayoutHeaderModule } from '../layout/layout-header';
 import { PdvValueModule } from '@shared/pipes/pdv-value';
 import { USER_PAGES } from './pages';
@@ -26,6 +28,7 @@ import { UserRoutingModule } from './user-routing.module';
   imports: [
     CommonModule,
     FormErrorModule,
+    FormsModule,
     InlineSVGModule,
     LayoutHeaderModule,
     MatExpansionModule,
@@ -34,6 +37,7 @@ import { UserRoutingModule } from './user-routing.module';
     MatFormFieldModule,
     MatInputModule,
     MatMenuModule,
+    MatSlideToggleModule,
     MatTabsModule,
     MatTooltipModule,
     NavigationModule,
@@ -53,6 +57,10 @@ import { UserRoutingModule } from './user-routing.module';
       provide: TRANSLOCO_SCOPE,
       useValue: 'user',
     },
+    {
+      provide: ToolbarStateService,
+      useClass: ToolbarStateService,
+    }
   ],
 })
 export class UserModule {
