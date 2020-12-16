2197547374f65e289fab8419c66608af5da24753
eugene.tishkevich <eugene.tishkevich@gmail.com>
2020-12-16 22:18:16 +0300

    feature: network: disable unavailable

--- projects/charon/src/app/core/services/api/index.ts
+++ projects/charon/src/app/core/services/api/index.ts
@@ -1,10 +1,13 @@
+import { PingService } from './ping';
 import { PostsApiService } from './posts';
 import { UserApiService } from './user';
 
+export * from './ping';
 export * from './posts';
 export * from './user';
 
 export const CORE_API_SERVICES = [
+  PingService,
   PostsApiService,
   UserApiService,
 ];
