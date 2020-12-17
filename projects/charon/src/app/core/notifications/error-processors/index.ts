46a7df0b95b2d3e548902ceb8afbec78f196af20
eugene.tishkevich <eugene.tishkevich@gmail.com>
2020-12-17 16:10:20 +0300

    feature: broadcast error handling

--- projects/charon/src/app/core/notifications/error-processors/index.ts
+++ projects/charon/src/app/core/notifications/error-processors/index.ts
@@ -1,7 +1,9 @@
-export * from './fallback-error-processor';
-
+import { BroadcastErrorProcessor } from './broadcast-error-processor';
 import { TranslatedErrorProcessor } from './translated-error-processor';
 
+export * from './fallback-error-processor';
+
 export const ERROR_PROCESSORS = [
+  BroadcastErrorProcessor,
   TranslatedErrorProcessor,
 ];
