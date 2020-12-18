d14df9b5cff6bebd8cb6121391fecc8e0a49bff7
eugene.tishkevich <eugene.tishkevich@gmail.com>
2020-12-18 23:51:08 +0300

    bugfix: commit missing files, PR#117

---
+++ projects/charon/src/app/hub/components/hub-post-pdv-diagram/hub-post-pdv-diagram.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
+import { SvgIconRegistry } from '@ngneat/svg-icon';
+import { svgDynamicsNegative, svgDynamicsPositive } from '../../../../../../../shared/svg-icons';
+
+@Component({
+  selector: 'app-hub-post-pdv-diagram',
+  templateUrl: './hub-post-pdv-diagram.component.html',
+  styleUrls: ['./hub-post-pdv-diagram.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HubPostPdvDiagramComponent {
+  @Input() public pdv: number;
+
+  constructor(svgIconRegistry: SvgIconRegistry) {
+    svgIconRegistry.register([
+      svgDynamicsNegative,
+      svgDynamicsPositive,
+    ]);
+  }
+
+  @HostBinding('class.mod-negative')
+  public get isNegative(): boolean {
+    return this.pdv < 0;
+  }
+
+  @HostBinding('class.mod-positive')
+  public get isPositive(): boolean {
+    return !this.isNegative;
+  }
+}
