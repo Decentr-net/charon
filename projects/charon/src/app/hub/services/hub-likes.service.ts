55d999efcc18a8d1bff9f4acfa0c9ad3705b835d
eugene.tishkevich <eugene.tishkevich@gmail.com>
2020-12-19 18:41:37 +0300

    bugfix: DEC-75,76: disable likes

--- projects/charon/src/app/hub/services/hub-likes.service.ts
+++ projects/charon/src/app/hub/services/hub-likes.service.ts
@@ -1,15 +1,33 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { LikeWeight, Post } from 'decentr-js';
 
+import { AuthService } from '../../core/auth';
 import { PostWithAuthor } from '../models/post';
 import { HubPostsService } from './hub-posts.service';
 
 @Injectable()
 export class HubLikesService {
+  public static isLikeUpdating: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
   constructor(
+    private authService: AuthService,
     private hubPostsService: HubPostsService,
   ) {
+  }
+
+  public canLikePost(postId: Post['uuid']): Observable<boolean> {
+    const post = this.hubPostsService.getPost(postId);
+    const walletAddress = this.authService.getActiveUserInstant().wallet.address;
+
+    if (post.owner === walletAddress) {
+      return of(false);
+    }
+
+    return HubLikesService.isLikeUpdating.pipe(
+      map((isUpdating) => !isUpdating),
+    );
   }
 
   public getPostChanges(postId: Post['uuid']): Observable<PostWithAuthor> {
@@ -17,6 +35,14 @@ export class HubLikesService {
   }
 
   public likePost(postId: Post['uuid'], likeWeight: LikeWeight): Observable<void> {
-    return this.hubPostsService.likePost(postId, likeWeight);
+    if (HubLikesService.isLikeUpdating.value) {
+      return;
+    }
+
+    HubLikesService.isLikeUpdating.next(true);
+
+    return this.hubPostsService.likePost(postId, likeWeight).pipe(
+      tap(() => HubLikesService.isLikeUpdating.next(false)),
+    );
   }
 }
