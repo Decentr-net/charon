41dc560464de3cbdd5ca01aeceac76e73534b691
eugene.tishkevich <eugene.tishkevich@gmail.com>
2020-12-20 21:14:19 +0300

    fix: remove console.log call

--- projects/charon/src/app/hub/models/post-category.ts
+++ projects/charon/src/app/hub/models/post-category.ts
@@ -3,8 +3,6 @@ import { PostCategory } from 'decentr-js';
 export const POST_CATEGORIES = Object.values(PostCategory)
   .filter((value) => !isNaN(+value)) as PostCategory[];
 
-console.log(POST_CATEGORIES);
-
 export const POST_CATEGORY_TRANSLATION_MAP = POST_CATEGORIES
   .reduce((acc, postCategory) => ({
     ...acc,
