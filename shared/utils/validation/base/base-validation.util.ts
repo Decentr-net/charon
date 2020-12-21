76f3e12eedd923d4eeb7417ab7792d05b2afb913
eugene.tishkevich <eugene.tishkevich@gmail.com>
2020-12-21 13:57:59 +0300

    feature: hub: post create: update validation

--- shared/utils/validation/base/base-validation.util.ts
+++ shared/utils/validation/base/base-validation.util.ts
@@ -54,4 +54,43 @@ export class BaseValidationUtil {
         : null
     };
   }
+
+  static minHtmlTextLength(minLength: number): ValidatorFn<string> {
+    return (control) => {
+      const html = control.value;
+      const textarea = document.createElement('textarea');
+      textarea.innerHTML = html;
+      const htmlTextLength = textarea.textContent.trim().length;
+
+      console.log(htmlTextLength);
+
+      return htmlTextLength < minLength
+        ? {
+          minHtmlTextLength: {
+            current: htmlTextLength,
+            min: minLength,
+          },
+        }
+        : null;
+    }
+  }
+
+  static maxStringBytes(maxBytes: number): ValidatorFn<string> {
+    return (control) => {
+      if (!control.value) {
+        return null;
+      }
+
+      const bytesLength = new TextEncoder().encode(control.value).length;
+
+      return bytesLength > maxBytes
+        ? {
+          maxBytes: {
+            current: bytesLength,
+            max: maxBytes,
+          },
+        }
+        : null;
+    }
+  }
 }
