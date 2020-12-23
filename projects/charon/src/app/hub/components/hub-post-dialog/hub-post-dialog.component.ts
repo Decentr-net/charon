69cd83132576e8785970cd55bda26fdc98f69951
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2020-12-23 18:47:15 +0300

    DEC-118: add remove post button to post dialog

--- projects/charon/src/app/hub/components/hub-post-dialog/hub-post-dialog.component.ts
+++ projects/charon/src/app/hub/components/hub-post-dialog/hub-post-dialog.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
+import { SvgIconRegistry } from '@ngneat/svg-icon';
 
 import { PostWithAuthor } from '../../models/post';
+import { svgTrash } from '@shared/svg-icons';
 
 @Component({
   selector: 'app-hub-post-dialog',
@@ -11,6 +13,10 @@ import { PostWithAuthor } from '../../models/post';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HubPostDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public post$: Observable<PostWithAuthor>) {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public post$: Observable<PostWithAuthor>,
+    private svgIconRegistry: SvgIconRegistry,
+  ) {
+    svgIconRegistry.register(svgTrash);
   }
 }
