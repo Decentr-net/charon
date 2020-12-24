ab7af7783f11ab63cadfbed69d62afbbc98d1759
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2020-12-24 21:32:00 +0300

    feature: DEC-127: type adjustmets

--- projects/charon/src/app/core/auth/models/auth-user.ts
+++ projects/charon/src/app/core/auth/models/auth-user.ts
@@ -2,9 +2,7 @@ import { User } from '@shared/services/auth';
 
 export interface AuthUser extends User {
   readonly emailConfirmed: boolean;
-  readonly firstName?: string;
   readonly isModerator: boolean;
-  readonly lastName?: string;
   readonly passwordHash: string;
   readonly primaryUsername?: string;
 }
