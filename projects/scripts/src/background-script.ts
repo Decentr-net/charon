21587198aca24c2acf2e6c595a3108aad42b486f
eugene.tishkevich <eugene.tishkevich@gmail.com>
2020-12-28 18:34:00 +0300

    remove dead code

--- projects/scripts/src/background-script.ts
+++ projects/scripts/src/background-script.ts
@@ -9,62 +9,22 @@ import {
   take,
   tap,
 } from 'rxjs/operators';
+import { PDVType } from 'decentr-js';
 
 import { AuthBrowserStorageService } from '../../../shared/services/auth';
 import { PDVUpdateNotifier } from '../../../shared/services/pdv';
 import { sendCookies } from './background/cookies/api';
 import { initMessageListeners, listenAllCookiesSet, listenLoginCookies } from './background/listeners';
 import { initAutoLock } from './background/lock';
-import { PDVType } from 'decentr-js';
-import { browser } from 'webextension-polyfill-ts';
+
+initAutoLock();
+
+initMessageListeners();
 
 const pdvUpdateNotifier = new PDVUpdateNotifier();
 pdvUpdateNotifier.start();
 
-initAutoLock();
-
-// const COOKIES_DEBOUNCE_MS = 500;
-
 const authStorage = new AuthBrowserStorageService();
-
-// authStorage.getActiveUser().pipe(
-//   switchMap((user) => {
-//     return user && user.registrationCompleted
-//       ? merge(
-//         listenRequestsOnCompletedWithBody({}, 'POST'),
-//         listenRequestsBeforeRedirectWithBody({}, 'POST'),
-//       ).pipe(
-//         filter(request => {
-//           return requestBodyContains(request.requestBody, [
-//             ...user.usernames,
-//             ...user.emails,
-//             user.primaryEmail,
-//           ]);
-//         }),
-//         debounceTime(COOKIES_DEBOUNCE_MS),
-//         switchMap(request => getCookies(new URL(request.url))),
-//         map((cookies) => ({ user, cookies })),
-//       )
-//       : EMPTY;
-//   }),
-//   mergeMap(({ user, cookies }) => {
-//     return sendCookies(
-//       user.wallet,
-//       PDVType.LoginCookie,
-//       cookies,
-//     ).pipe(
-//       retryWhen(errors => errors.pipe(
-//         delay(10000),
-//         take(60),
-//       )),
-//       catchError(() => EMPTY),
-//     );
-//   }),
-// ).subscribe(() => {
-//   pdvUpdateNotifier.notify();
-// });
-
-initMessageListeners();
 
 authStorage.getActiveUser().pipe(
   switchMap((user) => {
@@ -79,7 +39,6 @@ authStorage.getActiveUser().pipe(
         ),
         listenAllCookiesSet().pipe(
           map((cookies) => ({ cookies, pdvType: PDVType.Cookie })),
-          tap(console.log),
         ),
       ).pipe(
         mergeMap(({ cookies, pdvType}) => {
@@ -101,5 +60,3 @@ authStorage.getActiveUser().pipe(
 ).subscribe(() => {
   pdvUpdateNotifier.notify();
 });
-
-browser.cookies.onChanged.addListener(console.log);
