57db5b9d27ec4cc8feb6a80821b60aa0204b3bcd
eugene.tishkevich <eugene.tishkevich@gmail.com>
2021-01-05 19:07:06 +0300

    bugfix: user page: fix rising of balance requests

--- projects/charon/src/app/core/services/pdv/pdv.service.ts
+++ projects/charon/src/app/core/services/pdv/pdv.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { combineLatest, Observable } from 'rxjs';
-import { shareReplay, startWith, switchMap } from 'rxjs/operators';
+import { startWith, switchMap } from 'rxjs/operators';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { PDVDetails, PDVListItem, Wallet } from 'decentr-js';
 
@@ -44,7 +44,6 @@ export class PDVService extends NativePDVService {
       switchMap(([{ wallet, networkApi }]) => {
         return super.getBalance(networkApi, wallet.address);
       }),
-      shareReplay(1),
     );
   }
 
