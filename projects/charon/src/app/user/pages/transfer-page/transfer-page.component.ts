e01b68495c80d4f6e28540a4ec014506985d8706
eugene.tishkevich <eugene.tishkevich@gmail.com>
2021-01-14 23:16:57 +0300

    bugfix: DEC-159: user: fix navigation after transfer

--- projects/charon/src/app/user/pages/transfer-page/transfer-page.component.ts
+++ projects/charon/src/app/user/pages/transfer-page/transfer-page.component.ts
@@ -12,7 +12,7 @@ import { FORM_ERROR_TRANSLOCO_READ } from '@shared/components/form-error';
 import { NotificationService } from '@shared/services/notification';
 import { svgCheck, svgClose, svgLogoIcon } from '@shared/svg-icons';
 import { TransferPageService } from './transfer-page.service';
-import { Router } from '@angular/router';
+import { NavigationService } from '@core/navigation';
 import { SpinnerService } from '@core/services/spinner';
 import { TranslocoService } from '@ngneat/transloco';
 import { UserRoute } from '../../user.route';
@@ -45,8 +45,8 @@ export class TransferPageComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private formBuilder: FormBuilder,
+    private navigationService: NavigationService,
     private notificationService: NotificationService,
-    private router: Router,
     private transferPageService: TransferPageService,
     private spinnerService: SpinnerService,
     private svgIconRegistry: SvgIconRegistry,
@@ -124,7 +124,7 @@ export class TransferPageComponent implements OnInit {
         this.translocoService.translate('transfer_page.toastr.successful_sent', null, 'user'),
       );
 
-      this.router.navigate(['/', AppRoute.User]);
+      this.navigationService.back(['/', AppRoute.User])
     }, (error) => {
       this.notificationService.error(error);
     });
