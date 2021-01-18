a49456e713426edb4084402891c3407c2af59fa7
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-01-18 22:10:17 +0300

    feature: DEC-63: add loading to app

--- projects/charon/src/app/app.component.ts
+++ projects/charon/src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { filter, take } from 'rxjs/operators';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { SvgIconRegistry } from '@ngneat/svg-icon';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+import { svgLogoIcon } from '@shared/svg-icons';
+
+@UntilDestroy()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,30 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+  public isInitLoading = true;
+
+  constructor(
+    private router: Router,
+    svgIconRegistry: SvgIconRegistry,
+  ) {
+    svgIconRegistry.register([
+      svgLogoIcon,
+    ]);
+  }
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      take(1),
+      untilDestroyed(this),
+      filter(
+        event =>
+          event instanceof NavigationStart ||
+          event instanceof NavigationEnd ||
+          event instanceof NavigationCancel ||
+          event instanceof NavigationError,
+      ),
+    ).subscribe(() => {
+      this.isInitLoading = false;
+    });
+  }
 }
--- projects/charon/src/app/app.module.ts
+++ projects/charon/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { SvgIconsModule } from '@ngneat/svg-icon';
+import { TRANSLOCO_SCOPE, TranslocoModule } from '@ngneat/transloco';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -14,9 +16,17 @@ import { CoreModule } from './core';
     BrowserModule,
     CoreModule,
     HttpClientModule,
+    SvgIconsModule,
+    TranslocoModule,
   ],
   declarations: [
     AppComponent,
+  ],
+  providers: [
+    {
+      provide: TRANSLOCO_SCOPE,
+      useValue: 'core',
+    },
   ],
   bootstrap: [
     AppComponent,
