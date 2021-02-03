d958173dbe3097602409c83bdab6c2d0be97bf2b
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-02-03 16:03:16 +0300

    feature: DEC-198: toolbar: remove Recent navigation, change navigation from MyWall to Feed

--- projects/toolbar/src/app/services/navigation.service.ts
+++ projects/toolbar/src/app/services/navigation.service.ts
@@ -3,8 +3,7 @@ import { Injectable } from '@angular/core';
 import { MessageBus } from '@shared/message-bus';
 import { MessageCode } from '@scripts/messages';
 import { AppRoute as CharonAppRoute } from '@charon/app-route';
-import { HubFeedRoute as CharonHubFeedRoute, HubRoute as CharonHubRoute } from '@charon/hub';
-import { UserRoute as CharonUserRoute } from '@charon/user';
+import { HubRoute as CharonHubRoute } from '@charon/hub';
 
 @Injectable()
 export class NavigationService {
@@ -14,25 +13,16 @@ export class NavigationService {
     this.messageBus.sendMessage(MessageCode.ToolbarClose);
   }
 
-  public openCharonHubMyWall(): void {
+  public openCharonHubFeed(): void {
     this.messageBus.sendMessage(MessageCode.Navigate, [
       CharonAppRoute.Hub,
       CharonHubRoute.Feed,
-      CharonHubFeedRoute.MyWall,
     ]);
   }
 
   public openCharonHubOverview(): void {
     this.messageBus.sendMessage(MessageCode.Navigate, [
       CharonAppRoute.Hub,
-    ]);
-  }
-
-  public openCharonHubRecentNews(): void {
-    this.messageBus.sendMessage(MessageCode.Navigate, [
-      CharonAppRoute.Hub,
-      CharonHubRoute.Feed,
-      CharonHubFeedRoute.Recent,
     ]);
   }
 }
