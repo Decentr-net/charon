44d9cb221cd91ea186c4620fdbd8e538c57d023f
eugene.tishkevich <eugene.tishkevich@gmail.com>
2021-02-10 15:19:00 +0300

    feature: post card

--- projects/charon/src/app/hub/components/hub-post-card/hub-post-card.component.ts
+++ projects/charon/src/app/hub/components/hub-post-card/hub-post-card.component.ts
@@ -1,4 +1,14 @@
-import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  HostBinding,
+  Input,
+  OnChanges,
+  Renderer2,
+  SimpleChanges,
+} from '@angular/core';
+
 import { PostWithLike } from '../../models/post';
 
 @Component({
@@ -7,16 +17,27 @@ import { PostWithLike } from '../../models/post';
   styleUrls: ['./hub-post-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HubPostCardComponent {
+export class HubPostCardComponent implements OnChanges {
   @Input() public post: PostWithLike;
 
   @Input() public orientation: 'horizontal' | 'vertical' = 'vertical';
+
+  constructor(
+    private renderer: Renderer2,
+    private elementRef: ElementRef,
+  ) {
+    this.renderer.addClass(this.elementRef, `mod-${this.orientation}`);
+  }
 
   @HostBinding('class.mod-has-preview-image')
   public get hasPreviewImage(): boolean {
     return this.post && !!this.post.previewImage;
   }
 
-  @HostBinding('class.mod-horizontal')
-  public
+  public ngOnChanges({ orientation }: SimpleChanges): void {
+    if (orientation) {
+      this.renderer.removeClass(this.elementRef, `mod-${orientation.previousValue}`);
+      this.renderer.addClass(this.elementRef, `mod-${orientation.currentValue}`);
+    }
+  }
 }
