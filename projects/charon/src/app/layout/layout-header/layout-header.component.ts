258bbdadc1c8515cbd097692135da6f5168605eb
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-02-25 13:40:59 +0300

    bugfix: DEC-222: header logo: update logo, add navigation to homepage

--- projects/charon/src/app/layout/layout-header/layout-header.component.ts
+++ projects/charon/src/app/layout/layout-header/layout-header.component.ts
@@ -1,7 +1,11 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-
 import { MediaService } from '@core/services/media';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+import { isAuthorized$ } from '../../../../../scripts/src/content/auth';
+import { LockBrowserStorageService } from '@shared/services/lock';
+
+@UntilDestroy()
 @Component({
   selector: 'app-layout-header',
   templateUrl: './layout-header.component.html',
@@ -9,11 +13,29 @@ import { MediaService } from '@core/services/media';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LayoutHeaderComponent {
+  public isAuthorized: boolean;
+  public isLocked: boolean;
 
   @Input() background: 'grey' | 'white';
   @Input() selectNetwork: boolean;
   @Input() userProfile: boolean;
 
-  constructor(public mediaService: MediaService) {
+  constructor(
+    private lockBrowserStorageService: LockBrowserStorageService,
+    public mediaService: MediaService,
+  ) {
+    isAuthorized$().pipe(
+      untilDestroyed(this),
+    ).subscribe((isAuthorized) => {
+        this.isAuthorized = isAuthorized;
+      }
+    );
+
+    this.lockBrowserStorageService.getLockedChanges().pipe(
+      untilDestroyed(this),
+    ).subscribe((isLocked) => {
+        this.isLocked = isLocked;
+      }
+    );
   }
 }
