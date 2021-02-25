3f662fc9e82fac5df04da9bc04d02b32b6c14a76
eugene.tishkevich <eugene.tishkevich@gmail.com>
2021-02-25 17:17:05 +0300

    bugfix: network selection after browser reboot

--- projects/scripts/src/background/listeners.ts
+++ projects/scripts/src/background/listeners.ts
@@ -5,6 +5,8 @@ import { MessageBus } from '../../../../shared/message-bus';
 import { openCharonPage } from '../../../../shared/utils/navigation';
 import { MessageCode } from '../messages';
 import { initCharonAPIListeners } from './charon-api';
+import { mapTo, mergeMap } from 'rxjs/operators';
+import { NETWORK_READY_SUBJECT } from './network-switch';
 
 export const initMessageListeners = () => {
   const messageBus = new MessageBus();
@@ -16,6 +18,12 @@ export const initMessageListeners = () => {
   messageBus.onMessage(MessageCode.ToolbarClose).subscribe(() => {
     messageBus.sendMessageToCurrentTab(MessageCode.ToolbarClose);
   });
+
+  messageBus.onMessage(MessageCode.NetworkReady).pipe(
+    mergeMap((message) => NETWORK_READY_SUBJECT.pipe(
+      mapTo(message),
+    )),
+  ).subscribe((message) => message.sendResponse(void 0));
 
   browser.runtime.onConnect.addListener(noop);
 
