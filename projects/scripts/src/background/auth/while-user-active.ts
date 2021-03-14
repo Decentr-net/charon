1370c33f307b4be2a1748758e733b5e953b2534a
eugene.tishkevich <eugene.tishkevich@gmail.com>
2021-03-14 12:57:10 +0300

    bugfix: start cookies collection after user confirmation

--- projects/scripts/src/background/auth/while-user-active.ts
+++ projects/scripts/src/background/auth/while-user-active.ts
@@ -1,12 +1,11 @@
 import { AuthBrowserStorageService, User } from '../../../../../shared/services/auth';
 import { EMPTY, Observable } from 'rxjs';
-import { distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 
 const authStorage = new AuthBrowserStorageService();
 
 export const whileUserActive = <T>(observableFactory: (user: User) => Observable<T>) => {
   return authStorage.getActiveUser().pipe(
-    distinctUntilChanged((prev, curr) => prev?.id === curr?.id),
     switchMap((user) => user && user.registrationCompleted ? observableFactory(user) : EMPTY),
   );
 };
