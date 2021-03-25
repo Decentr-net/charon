1bbdeee7420c8f9928d467a1333e9cea084bc7f7
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-03-25 23:26:25 +0300

    feature: DEC-251: pdvValue pipe: remove 7th decimal, adjust microPDV decimal

--- shared/pipes/pdv-value/pdv-value.pipe.ts
+++ shared/pipes/pdv-value/pdv-value.pipe.ts
@@ -1,6 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
 
+import { MICRO_PDV_DIVISOR } from '../micro-value';
+
 @Pipe({
   name: 'pdvValue',
 })
@@ -11,11 +13,11 @@ export class PdvValuePipe implements PipeTransform {
   ) {
   }
 
-  public transform(value: string | number, convertToMu: boolean = false, digitsInfo: string = '1.7', digitsInfoMicro: string = '1.1'): string {
+  public transform(value: string | number, convertToMu: boolean = false, digitsInfo: string = '1.6', digitsInfoMicro: string = '1.0-1'): string {
     return value === 0
       ? '0'
       : convertToMu
-        ? this.decimalPipe.transform(Number(value) * 1000000, digitsInfoMicro) + 'μ'
+        ? this.decimalPipe.transform(Number(value) * MICRO_PDV_DIVISOR, digitsInfoMicro) + 'μ'
         : this.decimalPipe.transform(value, digitsInfo);
   }
 }
