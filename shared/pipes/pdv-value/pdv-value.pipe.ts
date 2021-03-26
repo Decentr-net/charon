b4df17178917c0ac18726b5fe960d3b242122d24
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-03-26 12:17:40 +0300

    feature: DEC-251: pdvValue pipe: convert input value to number

--- shared/pipes/pdv-value/pdv-value.pipe.ts
+++ shared/pipes/pdv-value/pdv-value.pipe.ts
@@ -14,10 +14,10 @@ export class PdvValuePipe implements PipeTransform {
   }
 
   public transform(value: string | number, convertToMu: boolean = false, digitsInfo: string = '1.6', digitsInfoMicro: string = '1.0-1'): string {
-    return value === 0
+    return Number(value) === 0
       ? '0'
       : convertToMu
         ? this.decimalPipe.transform(Number(value) * MICRO_PDV_DIVISOR, digitsInfoMicro) + 'μ'
-        : this.decimalPipe.transform(value, digitsInfo);
+        : this.decimalPipe.transform(Number(value), digitsInfo);
   }
 }
