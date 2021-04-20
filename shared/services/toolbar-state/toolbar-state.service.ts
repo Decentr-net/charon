596858e7511d3196759be17c418be091389de57b
eugene.tishkevich <eugene.tishkevich@gmail.com>
2021-04-20 17:43:45 +0300

    feature: DEC-292: disable toolbar by default

--- shared/services/toolbar-state/toolbar-state.service.ts
+++ shared/services/toolbar-state/toolbar-state.service.ts
@@ -1,11 +1,13 @@
-import { from, Observable } from 'rxjs';
-import { filter, mergeMap, startWith } from 'rxjs/operators';
+import { defer, Observable } from 'rxjs';
+import { distinctUntilChanged, map, mergeMap, startWith } from 'rxjs/operators';
 
 import { BrowserLocalStorage } from '../browser-storage';
 
 interface ToolbarState {
   enabled: boolean;
 }
+
+const TOOLBAR_DEFAULT_ENABLED = false;
 
 export class ToolbarStateService {
   private readonly toolbarStateStorage = BrowserLocalStorage
@@ -15,15 +17,13 @@ export class ToolbarStateService {
   private readonly enabledState$: Observable<boolean>;
 
   constructor() {
-    this.enabledState$ = from(this.toolbarStateStorage.get('enabled'))
-      .pipe(
-        filter((state) => typeof state !== 'undefined'),
-        startWith(true),
-        mergeMap((state) => this.toolbarStateStorage.onChange('enabled')
-          .pipe(
-            startWith(state),
-          )),
-      );
+    this.enabledState$ = defer(() => this.toolbarStateStorage.get('enabled')).pipe(
+      mergeMap((state) => this.toolbarStateStorage.onChange('enabled').pipe(
+        startWith(state),
+      )),
+      map((state) => state || TOOLBAR_DEFAULT_ENABLED),
+      distinctUntilChanged(),
+    );
   }
 
   public getEnabledState(): Observable<boolean> {
