ebc3bcfd2affe617df4e71ecd1cbc6224f39bb38
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-04-21 12:06:42 +0300

    feature: DEC-288: cd for tooltip

--- projects/charon/src/app/portal/components/pdv-rate-chart/tooltip/tooltip.component.ts
+++ projects/charon/src/app/portal/components/pdv-rate-chart/tooltip/tooltip.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-tooltip',
   templateUrl: './tooltip.component.html',
   styleUrls: ['./tooltip.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TooltipComponent {
   @Input() data!: Highcharts.TooltipFormatterContextObject;
