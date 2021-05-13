d854aa611c40665a1e90d79f793efffbe39da70d
eugene.tishkevich <eugene.tishkevich@gmail.com>
2021-05-13 18:26:34 +0300

    feature: DEC-327: remove login cookie collection

--- projects/scripts/src/background/cookies/events.ts
+++ projects/scripts/src/background/cookies/events.ts
@@ -1,37 +1,9 @@
-import { merge, Observable } from 'rxjs';
-import { debounceTime, switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { browser, Cookies } from 'webextension-polyfill-ts';
 import Cookie = Cookies.Cookie;
 import OnChangedChangeInfoType = Cookies.OnChangedChangeInfoType;
 
-import { ONE_SECOND } from '../../../../../shared/utils/date';
 import { hasOwnProperty } from '../../../../../shared/utils/object';
-import {
-  listenRequestsBeforeRedirectWithBody,
-  listenRequestsOnCompletedWithBody,
-  requestBodyContains
-} from '../requests';
-import { getBrowserCookies } from './browser';
-
-export const listenLoginCookies = (loginIdentifiers: string[]): Observable<Cookie[]> => {
-  const notEmptyStrings = loginIdentifiers.filter((str) => !!str);
-
-  return merge(
-    listenRequestsOnCompletedWithBody(
-      {},
-      'POST',
-      (requestBody) => requestBodyContains(requestBody, notEmptyStrings),
-    ),
-    listenRequestsBeforeRedirectWithBody(
-      {},
-      'POST',
-      (requestBody) => requestBodyContains(requestBody, notEmptyStrings),
-      ),
-  ).pipe(
-    debounceTime(ONE_SECOND),
-    switchMap(request => getBrowserCookies(new URL(request.url))),
-  );
-}
 
 export const listenCookiesSet = (filter: Partial<Cookie> = {}): Observable<Cookie> => {
   return new Observable((subscriber) => {
