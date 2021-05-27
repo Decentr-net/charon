f8e452ac7929fc60f30873a8032b762f5ac13b80
eugene.tishkevich <eugene.tishkevich@gmail.com>
2021-05-27 18:12:05 +0300

    feature: vpn: hide creds, ping after proxy enabled

--- projects/charon/src/app/core/services/proxy/proxy.service.ts
+++ projects/charon/src/app/core/services/proxy/proxy.service.ts
@@ -4,11 +4,15 @@ import { map } from 'rxjs/operators';
 
 import { Config, ConfigService } from '@shared/services/configuration';
 import { ExtensionProxySettings, getActiveProxySettings, setProxy } from '@shared/utils/browser';
+import { BlockchainNodeService } from '@shared/services/blockchain-node';
+import { NetworkService } from '../network';
 
 @Injectable()
 export class ProxyService {
   constructor(
     private configService: ConfigService,
+    private networkService: NetworkService,
+    private blockchainNodeService: BlockchainNodeService,
   ) {
   }
 
@@ -27,6 +31,12 @@ export class ProxyService {
   }
 
   public setProxy(host: string, port?: number): Promise<void> {
-    return setProxy({ host, port });
+    return setProxy({ host, port }).then(() => this.ping());
+  }
+
+  private ping(): Promise<void> {
+    const network = this.networkService.getActiveNetworkInstant();
+
+    return this.blockchainNodeService.getNodeAvailability(network.api).toPromise().then(() => void 0);
   }
 }
