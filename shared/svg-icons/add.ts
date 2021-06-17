a4bca90f2ff7817618356edc016ad419f78a4274
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-06-17 14:27:48 +0300

    feature: DEC-352: generate svg-icons

---
+++ shared/svg-icons/add.ts
@@ -0,0 +1,4 @@
+export const svgAdd = {
+    data: `<svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.6668 8.66668H8.66683V12.6667H7.3335V8.66668H3.3335V7.33334H7.3335V3.33334H8.66683V7.33334H12.6668V8.66668Z" fill="currentColor"/></svg>`,
+    name: 'add'
+};
---
+++ shared/svg-icons/send-coin.ts
@@ -0,0 +1,4 @@
+export const svgSendCoin = {
+    data: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up-right"><line x1="7" y1="17" x2="17" y2="7"/><polyline points="7 7 17 7 17 17"/></svg>`,
+    name: 'send-coin'
+};
