71510453e83e4c40ddeaf8dbe8a086c0429c64e4
Eugene <eugene.tishkevich@gmail.com>
2021-06-21 13:27:17 +0300

    feature: portal: update activity table (#298)

--- projects/charon/src/app/portal/pages/activity-page/activity-page.service.ts
+++ projects/charon/src/app/portal/pages/activity-page/activity-page.service.ts
@@ -1,17 +1,21 @@
 import { Injectable, OnDestroy } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { Observable } from 'rxjs';
 import { map, pluck, tap } from 'rxjs/operators';
+import { PDV, PDVType } from 'decentr-js';
 
 import { PDVService } from '@shared/services/pdv';
 import { coerceTimestamp } from '@shared/utils/date';
 import { InfiniteLoadingService } from '@shared/utils/infinite-loading';
-import { ActivityListItem } from './activity-page.definitions';
+import { uuid } from '@shared/utils/uuid';
+import { ACTIVITY_DATE_FORMAT, ActivityListItem } from './activity-page.definitions';
 
 @Injectable()
 export class ActivityPageService extends InfiniteLoadingService<ActivityListItem> implements OnDestroy {
   private readonly loadingCount: number = 20;
 
   constructor(
+    private datePipe: DatePipe,
     private pdvService: PDVService,
   ) {
     super();
@@ -29,10 +33,71 @@ export class ActivityPageService extends InfiniteLoadingService<ActivityListItem
       tap((pdvList) => pdvList.length < this.loadingCount && this.canLoadMore.next(false)),
       map((pdvList) => pdvList.map((pdvListItem) => ({
         timestamp: coerceTimestamp(pdvListItem),
-        pdvList: this.pdvService.getPDVDetails(pdvListItem).pipe(
+        pdvBlocks: this.pdvService.getPDVDetails(pdvListItem).pipe(
           pluck('pdv'),
+          map((pdvs) => this.groupPDVs(pdvs).map((pdvGroup) => ({
+            type: pdvGroup[0].type,
+            title: this.getPDVBlockTitle(pdvGroup[0]),
+            pdv: pdvGroup.map((pdv) => ({
+              title: this.getPDVTitle(pdv),
+              details: pdv,
+            })),
+          }))),
         ),
       }))),
     );
+  }
+
+  private groupPDVs(pdvs: PDV[]): PDV[][] {
+    const pdvMap = new Map<string, PDV[]>();
+
+    pdvs.forEach((pdv) => {
+      const key = this.getPDVGroupKey(pdv);
+      const group = pdvMap.get(key) || [];
+      pdvMap.set(key, [...group, pdv]);
+    });
+
+    return [...pdvMap.values()];
+  }
+
+  private getPDVGroupKey(pdv: PDV): string {
+    switch (pdv.type) {
+      case PDVType.Cookie:
+        return `${pdv.type}-${pdv.domain}`;
+      case PDVType.SearchHistory:
+        return `${pdv.type}-${pdv.engine}`;
+      default:
+        return uuid();
+    }
+  }
+
+  private getPDVBlockTitle(pdv: PDV): string {
+    switch (pdv.type) {
+      case PDVType.AdvertiserId:
+        return pdv.advertiser;
+      case PDVType.Cookie:
+        return pdv.domain;
+      case PDVType.Location:
+        return `${pdv.latitude}, ${pdv.longitude}`;
+      case PDVType.Profile:
+        return `${pdv.lastName} ${pdv.firstName}`;
+      case PDVType.SearchHistory:
+        return pdv.engine;
+    }
+  }
+
+  private getPDVTitle(pdv: PDV): string {
+    switch (pdv.type) {
+      case PDVType.AdvertiserId:
+        return pdv.id;
+      case PDVType.Cookie:
+        return pdv.name;
+      case PDVType.Location:
+        return pdv.timestamp;
+      case PDVType.Profile:
+        return `${pdv.lastName} ${pdv.firstName}`;
+      case PDVType.SearchHistory:
+        return this.datePipe.transform(pdv.timestamp, ACTIVITY_DATE_FORMAT);
+    }
   }
 }
