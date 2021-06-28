bc81ebf7b378814b35a64fc63e47b12f15a4335d
eugene.tishkevich <eugene.tishkevich@gmail.com>
2021-06-29 00:26:27 +0300

    feature: DEC-354: remove toolbar

--- projects/charon/src/app/portal/pages/portal-page/portal-page.component.ts
+++ projects/charon/src/app/portal/pages/portal-page/portal-page.component.ts
@@ -2,13 +2,11 @@ import { ChangeDetectionStrategy, Component, HostBinding, OnInit } from '@angula
 import { Observable } from 'rxjs';
 import { SvgIconRegistry } from '@ngneat/svg-icon';
 
-import { ToolbarStateService } from '@shared/services/toolbar-state';
 import { svgLogoIconOrange } from '@shared/svg-icons/logo-icon-orange';
 import { svgLogoPortal } from '@shared/svg-icons/logo-portal';
 import { svgWallet } from '@shared/svg-icons/wallet';
 import { isOpenedInTab } from '@shared/utils/browser';
 import {
-  AUTHORIZED_LAYOUT_FOOTER_SLOT,
   AUTHORIZED_LAYOUT_HEADER_ACTIONS_SLOT,
   AUTHORIZED_LAYOUT_HEADER_LOGO_SLOT,
   AUTHORIZED_LAYOUT_HEADER_META_SLOT,
@@ -28,19 +26,15 @@ export class PortalPageComponent implements OnInit {
   @HostBinding('class.mod-popup-view')
   public isOpenedInPopup: boolean = !isOpenedInTab();
 
-  public readonly footerSlot = AUTHORIZED_LAYOUT_FOOTER_SLOT;
   public readonly headerActionsSlot = AUTHORIZED_LAYOUT_HEADER_ACTIONS_SLOT;
   public readonly headerLogoSlot = AUTHORIZED_LAYOUT_HEADER_LOGO_SLOT;
   public readonly headerMetaSlot = AUTHORIZED_LAYOUT_HEADER_META_SLOT;
-
-  public toolbarEnabledState$: Observable<boolean>;
 
   public walletAddress$: Observable<string>;
 
   constructor(
     private portalPageService: PortalPageService,
     svgIconRegistry: SvgIconRegistry,
-    private toolbarStateService: ToolbarStateService
   ) {
     svgIconRegistry.register([
       svgLogoIconOrange,
@@ -50,13 +44,7 @@ export class PortalPageComponent implements OnInit {
   }
 
   public ngOnInit(): void {
-    this.toolbarEnabledState$ = this.toolbarStateService.getEnabledState();
-
     this.walletAddress$ = this.portalPageService.getWalletAddress();
-  }
-
-  public onToolbarEnabledStateChange(state: boolean): void {
-    this.toolbarStateService.setEnabledState(state);
   }
 
   public onWalletAddressCopied(): void {
--- projects/scripts/src/background/listeners.ts
+++ projects/scripts/src/background/listeners.ts
@@ -1,8 +1,4 @@
-import { noop } from 'rxjs';
-import { browser } from 'webextension-polyfill-ts';
-
 import { MessageBus } from '../../../../shared/message-bus';
-import { openCharonPage } from '../../../../shared/utils/navigation';
 import { MessageCode } from '../messages';
 import { initCharonAPIListeners } from './charon-api';
 import { mapTo, mergeMap } from 'rxjs/operators';
@@ -11,21 +7,11 @@ import { NETWORK_READY_SUBJECT } from './network-switch';
 export const initMessageListeners = () => {
   const messageBus = new MessageBus();
 
-  messageBus.onMessage(MessageCode.Navigate).subscribe(({ body: routes }) => {
-    openCharonPage(routes);
-  });
-
-  messageBus.onMessage(MessageCode.ToolbarClose).subscribe(() => {
-    messageBus.sendMessageToCurrentTab(MessageCode.ToolbarClose);
-  });
-
   messageBus.onMessage(MessageCode.NetworkReady).pipe(
     mergeMap((message) => NETWORK_READY_SUBJECT.pipe(
       mapTo(message),
     )),
   ).subscribe((message) => message.sendResponse(void 0));
-
-  browser.runtime.onConnect.addListener(noop);
 
   initCharonAPIListeners();
 }
