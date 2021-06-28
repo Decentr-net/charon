91b879d313f5e5734067b2c8900232aae8898c12
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-06-28 15:26:20 +0300

    feature: DEC-332: advertiserId PDV

--- projects/scripts/src/background/pdv/advertiser-id/collection.ts
+++ projects/scripts/src/background/pdv/advertiser-id/collection.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { AdvertiserPDV, PDVType } from 'decentr-js';
 import { filter, map } from 'rxjs/operators';
 
+import { ADVERTISERS } from './advertisers';
 import { listenCookiesSet } from '../cookies/events';
 
 export const listenAdvertiserPDVs = (): Observable<AdvertiserPDV> => {
@@ -9,26 +10,26 @@ export const listenAdvertiserPDVs = (): Observable<AdvertiserPDV> => {
     httpOnly: false,
     session: false,
   }).pipe(
-    filter((cookie) => {
-      return !!cookie.name.toLowerCase().match(/(^idfa_lower_md5)$/)
-        || !!cookie.name.toLowerCase().match(/(^idfa_lower_sha1)$/)
-        || !!cookie.name.toLowerCase().match(/(^idfa_upper_md5)$/)
-        || !!cookie.name.toLowerCase().match(/(^idfa_upper_md5)$/)
-        || !!cookie.name.toLowerCase().match(/(^idfa_upper_sha1)$/)
-        || !!cookie.name.toLowerCase().match(/(^gaid([0-9a-zA-Z_]+)?)+$/)
-        || !!cookie.name.toLowerCase().match(/(^aaid([0-9a-zA-Z_]+)?)+$/)
-        || !!cookie.name.toLowerCase().match(/(^adid([0-9a-zA-Z_]+)?)+$/)
-        || !!cookie.name.toLowerCase().match(/(^idfa([0-9a-zA-Z_]+)?)+$/)
-        || !!cookie.name.toLowerCase().match(/(^ppid([0-9a-zA-Z_]+)?)+$/);
-    }),
     map((cookie) => {
-      console.log(cookie);
+      const advertiser = ADVERTISERS.find((advertiser) => advertiser.domainRegex.test(cookie.domain));
+
+      if (!advertiser) {
+        return;
+      }
+
+      const isAdvertiserCookie = advertiser.cookieRegex.some((cookieRegex) => cookieRegex.test(cookie.name.toLowerCase()));
+
+      if (!isAdvertiserCookie) {
+        return;
+      }
 
       return {
         type: PDVType.AdvertiserId,
-        advertiser: cookie.name,
-        id: cookie.value,
-      };
+        advertiser: advertiser.advertiser,
+        name: cookie.name,
+        value: cookie.value,
+      } as AdvertiserPDV;
     }),
+    filter((PDV) => !!PDV),
   );
 };
