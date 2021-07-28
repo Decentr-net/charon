89e3794f18396e34247c1bef76b780ba8377867f
Alex Kh <8222330+sunzxs@users.noreply.github.com>
2021-07-28 12:21:19 +0300

    feature: DEC-415: add privacy-policy contextMenu (#317)

---
+++ projects/scripts/src/background/context-menu/policy.ts
@@ -0,0 +1,22 @@
+import { browser } from 'webextension-polyfill-ts';
+
+const PRIVACY_LINK = 'https://decentr.net/charon-privacy.html';
+
+const openPolicyPage = () => {
+  browser.tabs.create({
+    url: PRIVACY_LINK,
+  });
+};
+
+const init = () => {
+  browser.contextMenus.create({
+    id: 'privacy',
+    title: 'Privacy policy',
+    contexts: [
+      'browser_action',
+    ],
+    onclick: openPolicyPage,
+  });
+};
+
+export default init;
