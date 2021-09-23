ec436406545ad306b010dc350862427a18ad6a00
Eugene Tishkevich <eugene.tishkevich@gmail.com>
2021-09-23 05:34:02 +0300

    feature: image uploading: remove dead code

--- projects/charon/src/app/core/services/image-uploader/image-uploader.service.ts
+++ projects/charon/src/app/core/services/image-uploader/image-uploader.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { defer, Observable } from 'rxjs';
-import { mergeMap, pluck, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { pluck } from 'rxjs/operators';
 
 import { AuthService } from '@core/auth';
 import { ImageApiService } from '@core/services/api';
@@ -14,31 +14,6 @@ export class ImageUploaderService {
   }
 
   public upload(image: File): Observable<string> {
-    // return new Observable<string>((subscriber) => {
-    //   const reader = new FileReader();
-    //   reader.onloadend = () => {
-    //     subscriber.next(reader.result as string);
-    //   };
-    //   reader.readAsDataURL(image);
-    // }).pipe(
-    //   mergeMap((buf) => this.imageApiService.upload(buf as any, this.authService.getActiveUserInstant().wallet)),
-    //   pluck('hd')
-    // );
-
-    console.log(image);
-    //
-    // const buf = new Observable((sub) => {
-    //   const fileReader = new FileReader();
-    //   fileReader.onloadend = () => sub.next(fileReader.result as string);
-    //   fileReader.readAsBinaryString(image.slice());
-    // });
-    //
-    // return buf.pipe(
-    //   tap(console.log),
-    //   mergeMap((buf1: any) => this.imageApiService.upload(buf1, this.authService.getActiveUserInstant().wallet)),
-    //   pluck('hd'),
-    // );
-
     return this.imageApiService.upload(image, this.authService.getActiveUserInstant().wallet).pipe(
       pluck('hd'),
     );
