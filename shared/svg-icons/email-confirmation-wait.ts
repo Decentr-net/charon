f90bbf24d4d5e107b618baab92a3c17e1cd54ab5
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-09-23 15:21:58 +0300

    feature: DEC-450: email-confirmation page

---
+++ shared/svg-icons/email-confirmation-wait.ts
@@ -0,0 +1,4 @@
+export const svgEmailConfirmationWait = {
+    data: `<svg viewBox="0 0 96 96" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M80 16H16C11.6 16 8.04 19.6 8.04 24L8 72C8 76.4 11.6 80 16 80H45.1595C44.4051 77.4652 44 74.7799 44 72H16V32L48 52L80 32V45.1595C82.8897 46.0195 85.584 47.3335 88 49.0188V24C88 19.6 84.4 16 80 16ZM48 44L16 24H80L48 44ZM52 72C52 60.96 60.96 52 72 52C83.04 52 92 60.96 92 72C92 83.04 83.04 92 72 92C60.96 92 52 83.04 52 72ZM70 72.8L78.6 81.4L81.4 78.56L74 71.16V60H70V72.8Z" fill="currentColor"/></svg>`,
+    name: 'email-confirmation-wait'
+};
