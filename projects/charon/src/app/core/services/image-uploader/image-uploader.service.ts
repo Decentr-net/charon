f9d1d4ec5f5e200c5ab02228796385a104c0d5f1
Eugene <eugene.tishkevich@gmail.com>
2021-09-23 15:54:41 +0300

    feature: image uploading to BE (#332)

--- projects/charon/src/app/core/services/image-uploader/image-uploader.service.ts
+++ projects/charon/src/app/core/services/image-uploader/image-uploader.service.ts
@@ -2,18 +2,20 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { pluck } from 'rxjs/operators';
 
+import { AuthService } from '@core/auth';
 import { ImageApiService } from '@core/services/api';
 
 @Injectable()
 export class ImageUploaderService {
   constructor(
+    private authService: AuthService,
     private imageApiService: ImageApiService,
   ) {
   }
 
   public upload(image: File): Observable<string> {
-    return this.imageApiService.upload(image).pipe(
-      pluck('data', 'url'),
+    return this.imageApiService.upload(image, this.authService.getActiveUserInstant().wallet).pipe(
+      pluck('hd'),
     );
   }
 }
