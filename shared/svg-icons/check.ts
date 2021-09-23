2efb6dee3ef80484f651c15e3c1642d81f88819e
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-09-23 19:51:04 +0300

    feature: checkbox style adjustments

--- shared/svg-icons/check.ts
+++ shared/svg-icons/check.ts
@@ -1,4 +1,4 @@
 export const svgCheck = {
-    data: `<svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.06 5.55322L6.66667 9.94656L4.94 8.22656L4 9.16656L6.66667 11.8332L12 6.49989L11.06 5.55322Z" fill="currentColor"/></svg>`,
+    data: `<svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.4902 5.50008L12.0802 4.08008L6.49023 9.67008L3.91023 7.10008L2.49023 8.51008L6.49023 12.5001L13.4902 5.50008Z" fill="currentColor"/></svg>`,
     name: 'check'
 };
