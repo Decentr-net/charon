ec78b64510689b2d56ecd8c7315e1067287a8c04
Eugene Tishkevich <eugene.tishkevich@gmail.com>
2021-09-26 20:25:40 +0300

    feature: sign up: complete registration: 429 error text

--- projects/charon/src/app/sign-up/pages/complete-registration-page/complete-registration-page.service.ts
+++ projects/charon/src/app/sign-up/pages/complete-registration-page/complete-registration-page.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError, delay } from 'rxjs/operators';
+import { TranslocoService } from '@ngneat/transloco';
+import { StatusCodes } from 'http-status-codes';
 import { ProfileUpdate } from 'decentr-js';
 
 import { AuthService } from '@core/auth';
 import { UserService } from '@core/services';
+import { TranslatedError } from '@core/notifications';
 
 @Injectable()
 export class CompleteRegistrationPageService {
   constructor(
     private authService: AuthService,
+    private translocoService: TranslocoService,
     private userService: UserService,
   ) {
   }
@@ -22,6 +26,19 @@ export class CompleteRegistrationPageService {
       user.wallet,
     ).pipe(
       delay(100),
+      catchError((error) => {
+        switch (error?.response?.status) {
+          case StatusCodes.TOO_MANY_REQUESTS:
+            return throwError(new TranslatedError(
+              this.translocoService.translate(
+                `sign_up.complete_registration.toastr.errors.${StatusCodes.TOO_MANY_REQUESTS}`,
+                null,
+              ),
+            ));
+          default:
+            return throwError(error);
+        }
+      }),
     );
   }
 }
