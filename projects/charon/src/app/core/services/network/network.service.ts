1bce452b701a7549f2b81417fee949b65323f686
Eugene Tishkevich <eugene.tishkevich@gmail.com>
2021-09-27 18:18:47 +0300

    bugfix: app init: allow starting application with maintenance:true

--- projects/charon/src/app/core/services/network/network.service.ts
+++ projects/charon/src/app/core/services/network/network.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import { filter, first, mapTo } from 'rxjs/operators';
+import { merge } from 'rxjs';
 
+import { ConfigService } from '@shared/services/configuration';
 import { NetworkBrowserStorageService } from '@shared/services/network-storage';
 
 @Injectable()
 export class NetworkService {
   constructor(
+    private configService: ConfigService,
     private networkStorage: NetworkBrowserStorageService,
   ) {
   }
 
   public init(): Promise<void> {
-    return this.networkStorage.getActiveAPI().pipe(
+    return merge(
+      this.configService.getMaintenanceStatus(),
+      this.networkStorage.getActiveAPI(),
+    ).pipe(
       filter(Boolean),
       mapTo(void 0),
       first(),
