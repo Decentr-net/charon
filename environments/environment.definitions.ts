84e9d0f181f756e35f204b872a361bf80eb130ca
Eugene <eugene.tishkevich@gmail.com>
2021-09-29 22:53:06 +0300

    feature: ga (#339)

--- environments/environment.definitions.ts
+++ environments/environment.definitions.ts
@@ -2,6 +2,5 @@ export abstract class Environment {
   abstract avatars: string;
   abstract config: string;
   abstract currencyApi: string;
-  abstract ga: string;
   abstract production: boolean;
 }
