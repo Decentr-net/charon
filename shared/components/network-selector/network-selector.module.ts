9c778b30a68121de16b4bf477c77e8cb8cf91d20
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-09-30 20:07:19 +0300

    feature: NetworkSelectorService for CoreModule

--- shared/components/network-selector/network-selector.module.ts
+++ shared/components/network-selector/network-selector.module.ts
@@ -1,4 +1,4 @@
-import { ModuleWithProviders, NgModule, Type } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatMenuModule } from '@angular/material/menu';
 import { SvgIconsModule } from '@ngneat/svg-icon';
@@ -7,11 +7,6 @@ import { TypefaceModule } from '../../directives/typeface';
 import { ButtonModule } from '../button';
 import { SpinnerModule } from '../spinner';
 import { NetworkSelectorComponent } from './network-selector.component';
-import { NetworkSelectorService } from './network-selector.service';
-
-export interface NetworkSelectorModuleConfig {
-  service: Type<NetworkSelectorService>;
-}
 
 @NgModule({
   imports: [
@@ -30,15 +25,4 @@ export interface NetworkSelectorModuleConfig {
   ],
 })
 export class NetworkSelectorModule {
-  public static forRoot(config: NetworkSelectorModuleConfig): ModuleWithProviders<NetworkSelectorModule> {
-    return {
-      ngModule: NetworkSelectorModule,
-      providers: [
-        {
-          provide: NetworkSelectorService,
-          useClass: config.service,
-        },
-      ],
-    };
-  }
 }
