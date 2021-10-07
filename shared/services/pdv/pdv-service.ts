da9906604585a4a2a8c06e401fcfe0f619f339ac
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-10-08 02:08:20 +0300

    bugfix: microValue pipe usage

--- shared/services/pdv/pdv-service.ts
+++ shared/services/pdv/pdv-service.ts
@@ -76,7 +76,7 @@ export class PDVService {
     ]).pipe(
       map(([pDVs, rewards]) => (pDVs || []).reduce((acc, pdv) => acc + rewards[pdv.type] || 0, 0)),
       map((estimatedBalance) => this.microValuePipe.transform(estimatedBalance)),
-      map((balance) => balance || '0'),
+      map((balance) => balance ? balance.toString() : '0'),
       startWith('0'),
       distinctUntilChanged(),
     );
