2390676960ee209d392235589c7699b47cb6ac33
Eugene Tishkevich <eugene.tishkevich@gmail.com>
2021-10-31 08:12:33 +0300

    build: include source maps in dev mode

--- projects/scripts/webpack.config.dev.js
+++ projects/scripts/webpack.config.dev.js
@@ -3,6 +3,7 @@ const { config: baseConfig, manifestPluginFn } = require('./webpack.config.base'
 
 module.exports = webpackMerge(baseConfig, {
   mode: 'development',
+  devtool: 'eval-source-map',
   plugins: [
     manifestPluginFn({
       "content_security_policy": "script-src 'self' 'unsafe-eval' https://ssl.google-analytics.com; object-src 'self'",
