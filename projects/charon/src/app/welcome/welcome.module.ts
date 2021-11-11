b75568fe8a10a5cedcf214dc0d83a4f524d2d7fc
Alexander Khomovnenko <a.khomovnenko@aiolane.com>
2021-11-11 23:37:02 +0300

    feature: DEC-525: add network selector to Welcome/dHub pages

--- projects/charon/src/app/welcome/welcome.module.ts
+++ projects/charon/src/app/welcome/welcome.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { SvgIconsModule } from '@ngneat/svg-icon';
 import { TRANSLOCO_SCOPE, TranslocoModule } from '@ngneat/transloco';
 
+import { NetworkSelectorModule } from '@shared/components/network-selector';
 import { TypefaceModule } from '@shared/directives/typeface';
 import { WELCOME_PAGES } from './pages';
 import { WelcomeRoutingModule } from './welcome-routing.module';
@@ -11,6 +12,7 @@ import { WelcomeRoutingModule } from './welcome-routing.module';
     WELCOME_PAGES,
   ],
   imports: [
+    NetworkSelectorModule,
     SvgIconsModule,
     TranslocoModule,
     TypefaceModule,
