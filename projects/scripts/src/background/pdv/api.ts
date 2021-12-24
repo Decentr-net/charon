4e97ce5737fafb6e86bc9e8a7266282de1acb758
Eugene Tishkevich <eugene.tishkevich@gmail.com>
2021-12-24 18:10:26 +0300

    bugfix: DEC-557: pdv collection: reduce blacklist requests count

--- projects/scripts/src/background/pdv/api.ts
+++ projects/scripts/src/background/pdv/api.ts
@@ -1,5 +1,5 @@
 import { defer, Observable, of } from 'rxjs';
-import { catchError, mapTo, mergeMap } from 'rxjs/operators';
+import { catchError, mapTo, mergeMap, shareReplay } from 'rxjs/operators';
 import { getPDVBlacklist, PDV, PDVBlacklist, sendPDV as decentrSendPDV, Wallet } from 'decentr-js';
 
 import QUEUE, { QueuePriority } from '../queue';
@@ -7,15 +7,15 @@ import CONFIG_SERVICE from '../config';
 
 const configService = CONFIG_SERVICE;
 
-export const getBlacklist = (): Observable<PDVBlacklist> => {
-  return configService.getCerberusUrl().pipe(
+export const blacklist$: Observable<PDVBlacklist> =
+  configService.getCerberusUrl().pipe(
     mergeMap((cerberusUrl) => defer(() => getPDVBlacklist(cerberusUrl)).pipe(
       catchError(() => of({
         cookieSource: [],
       })),
     )),
+    shareReplay(1),
   );
-};
 
 export const sendPDV = (wallet: Wallet, pDVs: PDV[]): Observable<void> => {
   return defer(() => QUEUE.add(() => configService.getCerberusUrl().pipe(
