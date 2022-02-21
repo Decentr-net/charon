fc0217a48634b01f160bf69e815e3c611cb7aae6
Eugene Tishkevich <eugene.tishkevich@gmail.com>
2022-02-21 04:12:44 +0300

    feature: numeric directive: affect ng controls

--- shared/directives/numeric/numeric.directive.ts
+++ shared/directives/numeric/numeric.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit, Optional } from '@angular/core';
+import { NgControl } from '@angular/forms';
 import { fromEvent } from 'rxjs';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
@@ -12,6 +13,7 @@ export class NumericDirective implements OnInit {
   private previousValue: string;
 
   constructor(
+    @Optional() private control: NgControl,
     private elementRef: ElementRef<HTMLInputElement>,
   ) {
   }
@@ -98,6 +100,9 @@ export class NumericDirective implements OnInit {
   }
 
   private setValue(value: number | string): void {
-    this.elementRef.nativeElement.value = value.toString();
+    const valueToSet = value.toString();
+
+    this.elementRef.nativeElement.value = valueToSet;
+    this.control?.control.setValue(valueToSet);
   }
 }
