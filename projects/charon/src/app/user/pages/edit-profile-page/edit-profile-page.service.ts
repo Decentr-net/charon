98c776a60fce9c3689332db8634c8bf219b23cea
Eugene Tishkevich <eugene.tishkevich@gmail.com>
2022-02-22 19:47:09 +0300

    bugfix: edit profile: fix old password validator

--- projects/charon/src/app/user/pages/edit-profile-page/edit-profile-page.service.ts
+++ projects/charon/src/app/user/pages/edit-profile-page/edit-profile-page.service.ts
@@ -18,7 +18,7 @@ export class EditProfilePageService {
   public createCurrentPasswordValidAsyncValidator(): AsyncValidatorFn {
     return async (control) => {
       if (!control.value) {
-        return of(null);
+        return null;
       }
 
       const validPassword = await this.authService.validateCurrentUserPassword(control.value);
