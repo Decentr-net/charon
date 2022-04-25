caa30fb0e84c3a605e9410184cfc8e4442761803
Eugene Tishkevich <eugene.tishkevich@gmail.com>
2022-04-25 16:47:25 +0300

    bugfix: shared: formControlWarn: reset warnings at validation start

--- shared/forms/form-control-warn.ts
+++ shared/forms/form-control-warn.ts
@@ -6,4 +6,10 @@ export class FormControlWarn<T> extends FormControl<T> {
   public get isWarning(): boolean {
     return this.warnings !== null && this.warnings !== undefined;
   }
+
+  public override updateValueAndValidity(opts?: { onlySelf?: boolean; emitEvent?: boolean }) {
+    this.warnings = undefined;
+
+    super.updateValueAndValidity(opts);
+  }
 }
