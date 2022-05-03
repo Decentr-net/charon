189a8c9cd75bee63082569b0e31bd5015ef1ccc7
Eugene Tishkevich <eugene.tishkevich@gmail.com>
2022-05-03 17:34:44 +0300

    feature: DEC-614: pdv: add device property

--- projects/scripts/src/background/pdv/api.ts
+++ projects/scripts/src/background/pdv/api.ts
@@ -1,6 +1,6 @@
 import { defer, firstValueFrom, Observable, of, ReplaySubject, switchMap, take } from 'rxjs';
 import { catchError, map, mergeMap } from 'rxjs/operators';
-import { CerberusClient, PDV, PDVBlacklist, Wallet } from 'decentr-js';
+import { CerberusClient, PDV, PDVBlacklist, PDVDevice, Wallet } from 'decentr-js';
 
 import { NetworkBrowserStorageService } from '../../../../../shared/services/network-storage';
 import QUEUE, { QueuePriority } from '../queue';
@@ -26,7 +26,7 @@ export const blacklist$: Observable<PDVBlacklist> = (() => {
 
 export const sendPDV = (pDVs: PDV[], privateKey: Wallet['privateKey']): Observable<void> => {
   return defer(() => QUEUE.add(() => firstValueFrom(configService.getCerberusUrl().pipe(
-    mergeMap((cerberusUrl) => new CerberusClient(cerberusUrl).pdv.sendPDV(pDVs, privateKey)),
+    mergeMap((cerberusUrl) => new CerberusClient(cerberusUrl).pdv.sendPDV(pDVs, privateKey, PDVDevice.Desktop)),
     map(() => void 0),
   )), { priority: QueuePriority.PDV }));
 };
